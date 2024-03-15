2a0768a72a8da41a2cce24f3148486f035df201c
Michael Hofweller <michael.hofweller@gmail.com>
2024-03-14 21:44:36 -0700

    fixing prod db url connection

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // env: {
-    //     DATABASE_URL:
-    //         process.env.DATABASE_URL ||
-    //         'postgresql://postgres:G-3ec3G5aFb5BC1Bga6EE3e4d-6f1FBg@postgres.railway.internal:5432/railway',
-    // },
+    env: {
+        DATABASE_URL:
+            process.env.DATABASE_URL ||
+            'postgresql://postgres:G-3ec3G5aFb5BC1Bga6EE3e4d-6f1FBg@postgres.railway.internal:5432/railway',
+    },
 };
 
 export default nextConfig;
