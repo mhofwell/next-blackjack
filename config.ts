3a2a38b37587e5713ede984d5e4383fa526dec6a
Michael Hofweller <michael.hofweller@gmail.com>
2024-03-30 11:25:45 -0700

    test url access

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 let SERVER_URL: string = '';
 
 if (process.env.NODE_ENV === 'production') {
-    SERVER_URL = `http:${process.env.RAILWAY_PRIVATE_DOMAIN}:${process.env.PORT}/api/graphql`;
+    SERVER_URL = `http://${process.env.RAILWAY_PRIVATE_DOMAIN}:${process.env.PORT}/api/graphql`;
 } else {
     SERVER_URL = `http://localhost:3000/api/graphql`;
 }
--- lib/actions/getDbCredentials.ts
+++ lib/actions/getDbCredentials.ts
@@ -2,6 +2,7 @@ import { getClient } from '../apollo/client';
 import { DB_QUERY } from '../graphql/queries';
 
 export async function getDbCredentials() {
+    // this server action has access to server env vars and can make requests
     const { data, errors } = await getClient().query({
         query: DB_QUERY,
     });
