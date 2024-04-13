f745d4b19888fbc95cf3287e14f1b13a262fe4da
Michael Hofweller <michael.hofweller@gmail.com>
2024-04-13 10:18:55 -0700

    fix odd spacing in browser

--- app/pool/[id]/page.tsx
+++ app/pool/[id]/page.tsx
@@ -4,8 +4,8 @@ import Footer from '@/components/Footer';
 export default function Page() {
     return (
         <div className="flex justify-center bg-no-repeat h-screen">
-            <div className="flex flex-col">
-                <div className="w-full mt-5 min-h-[400px] dark:bg-gray-900 border bg-white border-gray-800 rounded-xl ">
+            <div className="flex flex-col ">
+                <div className="w-full mt-5 dark:bg-gray-900 border bg-white border-gray-800 rounded-xl ">
                     <div className="mx-auto max-w-7xl p-5 ">
                         {/* <p className="text-xl dark:text-white text-gray-800">
                         Entries
